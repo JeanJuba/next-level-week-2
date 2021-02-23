5af35909c44b4d12f1dfd13485b1e69e5e759fa3
Jean Juba <jeanwsj96@hotmail.com>
2021-02-23 19:58:15 -0300

    Concluir backend

---
+++ server/README.md
@@ -0,0 +1,12 @@
+# Funcionalidades
+
+# Conexões
+
+- Rota para total de conexões realizadas
+- Rota para criar uma nova conexão
+
+# Aulas
+
+- Rota para criar aula
+- Listar aulas
+    - Filtrar por matéria, dia semana e horário
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
-
-app.get('/users', (request, response) => {
-    const users = [{name: 'Diego', age: 25}, {name: 'Vini', age: 21}]
-   return response.json(users);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
 
